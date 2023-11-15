4c193f85a9160d663bbf5de3113c2973f86a32d1
neal-garcia95 <neal@builtbymasonry.com>
2023-11-14 20:17:54 -0600

    Pause button

--- js/app.js
+++ js/app.js
@@ -11,14 +11,17 @@ function updateTimer(){
 function startTimer() {
     if (!isRunning) {
       isRunning = true;
+      // Set interval for every 1000 ms (1 sec)
       timer = setInterval(() => {
+        // If timer reaches zero
         if (minutes === 0 && seconds === 0) {
           clearInterval(timer);
           isRunning = false;
           alert('Time is up!');
         } else {
+          // Count down
           if (seconds === 0) {
-            minutes--;
+            minutes--; // Decrement minutes once seconds reaches zero
             seconds = 59;
           } else {
             seconds--;
@@ -28,6 +31,11 @@ function startTimer() {
       }, 1000);
     }
 }
+
+function pauseTimer() {
+    clearInterval(timer);
+    isRunning = false;
+  }
   
 function resetTimer() {
     clearInterval(timer);
@@ -38,6 +46,7 @@ function resetTimer() {
 }
 
 document.getElementById('startButton').addEventListener('click', startTimer);
+document.getElementById('pauseButton').addEventListener('click', pauseTimer);
 document.getElementById('resetButton').addEventListener('click', resetTimer);
 
 // Initialize the timer display
